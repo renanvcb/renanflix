b93191eb19b2b7324db86ea4bf68a32796370868
renanvcb <renanvcb@gmail.com>
2020-07-29 20:00:14 -0300

    Simple form added

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -7,6 +7,15 @@ function CadastroCategoria() {
         <PageDefault>
             <h1 style={{color: 'var(--primary)'}}>Nova categoria</h1>
 
+            <form>
+                <label>
+                    Nome da categoria:
+                    <input type="text"/>
+                </label>
+
+                <button>Cadastrar</button>
+            </form>
+
             <Link to='/'>
                 Voltar para a Home
             </Link>
