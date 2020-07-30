e971152935e787ba2f7ceec11c09ec686a0b73c4
renanvcb <renanvcb@gmail.com>
2020-07-30 17:54:40 -0300

    New formfield. But needs an upgrade

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,19 +1,38 @@
 import React from 'react';
 
 function FormField({ label, type, name, value, onChange }) {
-    return (
-        <div>
-            <label>
-                {`${label}: `}
-                <input
-                    type={type}
-                    name={name}
-                    value={value}
-                    onChange={onChange}
-                />
-            </label>
-        </div>
-    );
+
+    if (type === 'textarea') {
+        return (
+            <div>
+                <label>
+                    {`${label}: `}
+                    <textarea
+                        type='text'
+                        name={name}
+                        value={value}
+                        onChange={onChange}
+                    />
+                </label>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <label>
+                    {`${label}: `}
+                    <input
+                        type={type}
+                        name={name}
+                        value={value}
+                        onChange={onChange}
+                    />
+                </label>
+            </div>
+        );
+    }
+
+    
 }
 
 export default FormField;
